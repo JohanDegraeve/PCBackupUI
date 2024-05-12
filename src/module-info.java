bfb875cfee6a23aacb731f82f833c66a9aefe0c7
Johan Degraeve <johan.degraeve@gmail.com>
2024-05-12 17:19:20 +0200

    files need to build with maven

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module PCBackupUI {
+	requires com.fasterxml.jackson.databind;
+    requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	exports main;
+}
