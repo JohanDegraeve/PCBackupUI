56b272df20c48a93e812ec55f9b4087c43a25c68
Johan Degraeve <johan.degraeve@gmail.com>
2024-05-12 23:01:45 +0200

    this allows to build the jar with eclipse and runs with this command
    java --module-path /Users/johandegraeve/Downloads/javafx-sdk-22/lib --add-modules javafx.controls,javafx.fxml -jar PCBackupUI.jar
    
    - jackson jars are downloaded here :
      - https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations/2.17.1
      - https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.17.1
      - https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core/2.17.1
    
    - javafx sdk downloaded here
    https://gluonhq.com/products/javafx/

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,6 @@
 module PCBackupUI {
+	requires com.fasterxml.jackson.annotation;
+	requires com.fasterxml.jackson.core;
 	requires com.fasterxml.jackson.databind;
     requires javafx.controls;
 	requires javafx.fxml;
